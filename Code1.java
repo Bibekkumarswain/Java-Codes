d8c4e734b35c1b30d6566dd6b17b686785017bfc
Bibekkumarswain <bibekkumarswain560@gmail.com>
2025-07-21 23:22:12 +0530

    add both code1 and code2

---
+++ Code1.java
@@ -0,0 +1,9 @@
+package Variables;
+
+class Code1
+{
+    public static void main(String a[])
+    {
+        System.out.println("Hello Java Ocean");
+    }
+}
---
+++ Code2.java
@@ -0,0 +1,10 @@
+package Variables;
+
+public class Code2 
+{
+    //public static void login()
+    public static void main(String a[])
+    {
+        System.out.println("Login Successful");
+    }
+}
