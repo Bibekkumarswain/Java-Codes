35a6f2754f9ef28ab1e6fac0f090d7f43768d08e
Bibekkumarswain <bibekkumarswain560@gmail.com>
2025-07-25 21:29:50 +0530

    print data type int,double,boolean

---
+++ Code3.java
@@ -0,0 +1,16 @@
+package Variables;
+
+class Code3
+{
+    int a=10;
+    static double b=15.9;
+    public static void main(String[] args)
+    {
+        boolean c=true;
+
+        Code3 i= new Code3();
+        System.out.println(i.a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
