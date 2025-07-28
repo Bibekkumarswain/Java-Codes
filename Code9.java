c10ab2faebdbb4c653baa558348aea56b836b391
Bibekkumarswain <bibekkumarswain560@gmail.com>
2025-07-28 15:23:49 +0530

    Check greatest number

--- Code9.java
+++ Code9.java
@@ -1,3 +1,22 @@
-public class Code9 {
-    
+package ConditionalStmt;
+
+import java.util.Scanner;
+
+public class Code9{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter two number:");
+
+        int firstNum = scn.nextInt();
+        int SecondNum = scn.nextInt();
+        if(firstNum>SecondNum){
+            System.out.println("first number is bigger than second");
+        }
+        else if(SecondNum>firstNum){
+            System.out.println("second number is bigger than first");
+        }
+        else{
+            System.out.println("Entered number are equal.");
+        }
+    }
 }
