18cc21aceae41d2f7751d085c002dd9ceff56273
Bibekkumarswain <bibekkumarswain560@gmail.com>
2025-07-29 14:38:54 +0530

    Add for-loop code

--- Code10.java
+++ Code10.java
@@ -1,5 +1,18 @@
+//Simple example of for-loop
 package ConditionalStmt;
 
+import java.util.Scanner;
+
 public class Code10 {
-    
+    public static void main(String[] a)
+    {
+        int num;
+        System.out.print("Enter any number:");
+        Scanner scn=new Scanner(System.in);
+        num=scn.nextInt();
+
+        for(int i=1;i<=10;i++){
+            System.out.println(num*i);
+        }
+    }
 }
