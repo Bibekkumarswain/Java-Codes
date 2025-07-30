855b26f0f3cdb3160d938e2d573c008e89465f75
Bibekkumarswain <bibekkumarswain560@gmail.com>
2025-07-30 13:49:36 +0530

    Control flow syntax

---
+++ Code14.java
@@ -0,0 +1,15 @@
+//Control flow(break,continue,return) syntax
+package ConditionalStmt;
+
+public class Code14 {
+    public static void main(String[] a)
+    {
+        for(int i=1;i<=10;++i)
+        {
+            if(i==5)
+            // break;
+            continue;
+            System.out.print(i+" ");
+        }
+    }
+}
