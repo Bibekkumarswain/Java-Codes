57ecfc93df474d94b67d8022310308d5facae1ff
Bibekkumarswain <bibekkumarswain560@gmail.com>
2025-07-30 13:36:50 +0530

    update code file

--- Code12.java
+++ Code12.java
@@ -1,5 +1,24 @@
 package ConditionalStmt;
 
+import java.util.Scanner;
+
 public class Code12 {
-    
+    public static void main(String[] a){
+        
+        int num;
+        System.out.print("Enter a number:");
+        Scanner scn = new Scanner(System.in);
+
+        num = scn.nextInt();
+        while(num>=0){
+            if(num%2==0){
+                System.out.println("Even number");
+                break;
+            }
+            else{
+                System.out.println("odd number");
+                break;
+            }
+        }
+    }
 }
--- Code13.java
+++ Code13.java
@@ -1,5 +1,23 @@
+//To print 
 package ConditionalStmt;
 
 public class Code13 {
-    
+    public static void main(String[] args) {
+
+        // Using while loop
+        System.out.println("Using while loop:");
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+
+        System.out.println(); // for a new line
+
+        // Using for loop
+        System.out.println("Using for loop:");
+        for (int j = 1; j <= 10; j++) {
+            System.out.print(j + " ");
+        }
+    }
 }
